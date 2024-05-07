91e0eb47dd40b145a056c982dd882bb8d32f27ad
szogabaha <szolnokgabcsi@gmail.com>
2024-05-07 18:33:52 +0200

    handle ctrlc sigkill signals

--- src/actuators/actuators/__init__.py
+++ src/actuators/actuators/__init__.py
@@ -5,9 +5,12 @@ from actuators.hil_driver import HiLDriver
 import rclpy
 from rclpy.executors import MultiThreadedExecutor
 from pymycobot import MyCobotSocket
+from signal import signal, SIGINT
+import sys
 
 
 def start_mock_driver(args=None):
+    signal(SIGINT, signal_handler)
     rclpy.init(args=args)
     driver_node = MockMyCobotDriver()
     executor = MultiThreadedExecutor()
@@ -16,6 +19,7 @@ def start_mock_driver(args=None):
     rclpy.shutdown()
 
 def start_driver(args=None):
+    signal(SIGINT, signal_handler)
     mc = MyCobotSocket("192.168.0.223",9001)
     rclpy.init(args=args)
     driver_node = MyCobotDriver(mc = mc)
@@ -25,9 +29,17 @@ def start_driver(args=None):
     rclpy.shutdown()
 
 def start_hil(args=None):
+    signal(SIGINT, signal_handler)
     rclpy.init(args=args)
     driver_node = HiLDriver()
     executor = MultiThreadedExecutor()
     executor.add_node(driver_node)
     executor.spin()
     rclpy.shutdown()
+
+
+
+def signal_handler(sig, frame):
+    rclpy.shutdown()
+    # Perform cleanup actions here
+    sys.exit(0)
--- src/camera_controller/camera_controller/__init__.py
+++ src/camera_controller/camera_controller/__init__.py
@@ -4,6 +4,8 @@ from camera_controller.usb_view_fetcher import USBViewFetcher
 from camera_controller.mock_view_fetcher import MockViewFetcher
 from camera_controller.ip_server_view_fetcher import IPServerViewFetcher
 import rclpy
+from signal import signal, SIGINT
+import sys
 
 STARTSPACE_TOPIC = "/spinningfactory/startspace_state"
 WORKSPACE_TOPIC = "/spinningfactory/workspace_state"
@@ -11,6 +13,7 @@ URL = "http://192.168.151.74:8080/shot.jpg"
 
 
 def start_mock_camera(args=None):
+    signal(SIGINT, signal_handler)
     rclpy.init(args=args)
     camera = MockViewFetcher()
     camera_node = CameraDriver("startspace_mock", STARTSPACE_TOPIC, camera)
@@ -19,6 +22,7 @@ def start_mock_camera(args=None):
 
 
 def start_ip_server_camera(args=None):
+    signal(SIGINT, signal_handler)
     rclpy.init(args=args)
     camera = IPServerViewFetcher(URL)
     camera_node = CameraDriver("startspace_IPServer", STARTSPACE_TOPIC, camera)
@@ -26,6 +30,7 @@ def start_ip_server_camera(args=None):
     rclpy.shutdown()
 
 def start_usb_camera(args=None):
+    signal(SIGINT, signal_handler)
     rclpy.init(args=args)
     camera = USBViewFetcher(cam_port=2)
     camera_node = CameraDriver("startspace_USB", STARTSPACE_TOPIC, camera)
@@ -35,6 +40,7 @@ def start_usb_camera(args=None):
     rclpy.shutdown()
 
 def work_mock_camera(args=None):
+    signal(SIGINT, signal_handler)
     rclpy.init(args=args)
     camera = MockViewFetcher()
     camera_node = CameraDriver("workspace_mock", WORKSPACE_TOPIC, camera)
@@ -43,6 +49,7 @@ def work_mock_camera(args=None):
 
 
 def work_ip_server_camera(args=None):
+    signal(SIGINT, signal_handler)
     rclpy.init(args=args)
     camera = IPServerViewFetcher(URL)
     camera_node = CameraDriver("workspace_IPServer", WORKSPACE_TOPIC, camera)
@@ -50,8 +57,16 @@ def work_ip_server_camera(args=None):
     rclpy.shutdown()
 
 def work_usb_camera(args=None):
+    signal(SIGINT, signal_handler)
     rclpy.init(args=args)
     camera = USBViewFetcher(cam_port=2)
     camera_node = CameraDriver("workspace_USB", WORKSPACE_TOPIC, camera)
     rclpy.spin(camera_node)
     rclpy.shutdown()
+
+
+
+def signal_handler(sig, frame):
+    rclpy.shutdown()
+    # Perform cleanup actions here
+    sys.exit(0)
